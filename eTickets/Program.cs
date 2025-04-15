f0ff9343e9ee27e9943573f3f76dc1c5d782af80
jocatins <jocakhamen@gmail.com>
2025-04-15 22:56:13 +0100

    initial data seeding

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,16 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// DBContext Configuration
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
@@ -23,5 +32,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+AppDbInitializer.Seed(app);
 
 app.Run();
